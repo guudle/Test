a2a6969d4a5136c8232db219bcad9fc911b1251a
guudle <guudle100@gmail.com>
2018-07-13 10:09:15 +0300

    Update Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -16,7 +16,7 @@ public class Test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Using Netbeans with GitHub");
+        System.out.println("Using Netbeans with Git");
     }
     
 }
